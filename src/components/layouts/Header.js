52dd7fcb106155d8e97d614dfc7cc20d27abf59a
Robert <rkrause815@gmail.com>
2020-02-11 16:41:10 -0600

    Changed Header

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,47 +1,47 @@
-import React, { Component } from 'react';
-import './Header.scss';
+import React, { Component } from "react";
+import "./Header.scss";
 
 export default class Header extends Component {
   render() {
     return (
-      <header id='about'>
-        <div className='container'>
-          <div className='heading-content'>
-            <h1 data-aos='fade-left'>Robert Krause</h1>
-            <h3 data-aos='fade-right'>Front-End Web Developer</h3>
-            <p data-aos='fade-up'>
+      <header id="about">
+        <div className="container">
+          <div className="heading-content">
+            <h1 data-aos="fade-left">Robert Krause</h1>
+            <h3 data-aos="fade-right">Front-End Web Developer</h3>
+            <p data-aos="fade-up">
               I'm a Front-End Web Developer from the Greater Chicago area. I
               have a passion for creating quality websites of all types. I love
               making websites using HTML, CSS, and JavaScript. I'm currently
-              enjoying my time learning React. Feel free to contact me if you
-              want to talk. I'd love to hear from you.
+              enjoying my time learning web development. Feel free to contact me
+              if you want to talk. I'd love to hear from you.
             </p>
           </div>
           <div
-            className='social-links'
-            data-aos='fade-left'
-            data-aos-offset='80'
+            className="social-links"
+            data-aos="fade-left"
+            data-aos-offset="80"
           >
             <a
-              href='https://github.com/rkrause4'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://github.com/rkrause4"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <i className='fab fa-github'></i>
+              <i className="fab fa-github"></i>
             </a>
             <a
-              href='https://www.linkedin.com/in/robert-krause-14b195167/'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://www.linkedin.com/in/robert-krause-14b195167/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <i className='fab fa-linkedin-in'></i>
+              <i className="fab fa-linkedin-in"></i>
             </a>
             <a
-              href='https://codepen.io/Rob10'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://codepen.io/Rob10"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <i className='fab fa-codepen'></i>
+              <i className="fab fa-codepen"></i>
             </a>
           </div>
         </div>
